de7c473b926cca6ae8d9e7d60bc5afe27613f18f
php 04 <php.04@xuxu.cec.ime.usp.br>
2018-01-23 21:16:11 -0200

    Formulario 2

---
+++ salvarCliente.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cadastro do cliente 2</title>
+  </head>
+  <body>
+    <?php
+      require 'clienteForm.php';
+
+      $cliente = new ClienteForm();
+      $cliente->nome = $_POST['nome'];
+      $cliente->email = $_POST['email'];
+
+      $servidor = "localhost";
+      $usuario = "root";
+      $senha = "senha";
+      $banco = "novobd";
+
+      $conexao = new mysqli($servidor, $usuario, $senha, $banco) or die("Erro de conexao: $conexao->connect_error");
+
+      $sql = "INSERT INTO clientes VALUES
+              (null, '$cliente->nome', '$cliente->email')";
+
+      $saida = $conexao->query($sql);
+?>
+
+  <h1>Cliente</h1>
+
+  <?php if($saida === true){?>
+      <h3>Cliente: <em><?= $cliente->nome ?></em>, email: <em><?= $cliente->email ?></em> salvo com sucesso</h3>
+
+  <?php } else { ?>
+
+      <h3>erro: <?= $conexao->error ?> </h3>
+
+    <?php };
+
+      $conexao->close();
+
+      ?>
+
+
+
+
+  </body>
+</html>
